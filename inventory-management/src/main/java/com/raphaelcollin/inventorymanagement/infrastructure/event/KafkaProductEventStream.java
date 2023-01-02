adcb70294949a7e9b7c9259f796ae276948ae997
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2023-01-02 15:03:40 -0300

    Create Kafka Producer

---
+++ inventory-management/src/main/java/com/raphaelcollin/inventorymanagement/infrastructure/event/KafkaProductEventStream.java
@@ -0,0 +1,44 @@
+package com.raphaelcollin.inventorymanagement.infrastructure.event;
+
+import com.raphaelcollin.inventorymanagement.application.inventorymanager.WriteOnlyProductStream;
+import com.raphaelcollin.inventorymanagement.domain.event.ProductEvent;
+import com.raphaelcollin.inventorymanagement.kafka.InventoryProduct;
+import com.raphaelcollin.inventorymanagement.kafka.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class KafkaProductEventStream implements WriteOnlyProductStream {
+    private final KafkaTemplate<String, Product> kafkaTemplate;
+    private final String productsTopic;
+
+    public KafkaProductEventStream(KafkaTemplate<String, Product> kafkaTemplate, @Value("${kafka.products-topic}") String productsTopic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.productsTopic = productsTopic;
+    }
+
+    @Override
+    public void publishEvent(final ProductEvent event) {
+        kafkaTemplate.send(productsTopic, event.getId(), createKafkaObjectFromDomain(event));
+    }
+
+    private Product createKafkaObjectFromDomain(ProductEvent event) {
+        Product product = new Product();
+        product.setId(event.getId());
+        product.setCorrelationId(event.getCorrelationId());
+        product.setName(event.getName());
+        product.setDescription(event.getDescription());
+        product.setAdditionalInfo(event.getAdditionalInfo());
+        product.setPrice((float) event.getPrice());
+        product.setTotalAmountAvailable(event.getTotalAmountAvailable());
+        product.setIsAvailable(event.isAvailable());
+        product.setInventoryProducts(event.getInventoryProducts().stream()
+                .map(i -> new InventoryProduct(i.getInventoryId(), i.getQuantity()))
+                .collect(Collectors.toUnmodifiableList())
+        );
+        return product;
+    }
+}
