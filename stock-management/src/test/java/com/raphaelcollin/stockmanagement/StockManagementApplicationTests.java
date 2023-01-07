b9e138e6b6ca2c0ca12db5eafe00891d3b0f6bbc
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2023-01-07 13:58:49 -0300

    Create Stock Management Module

---
+++ stock-management/src/test/java/com/raphaelcollin/stockmanagement/StockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelcollin.stockmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
