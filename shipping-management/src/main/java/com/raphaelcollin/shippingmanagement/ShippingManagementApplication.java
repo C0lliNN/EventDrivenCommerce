d8bb253dd13a6b1e90dd89a534eafd04d83691cc
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2023-01-10 08:58:51 -0300

    Create Shipping Management Module

---
+++ shipping-management/src/main/java/com/raphaelcollin/shippingmanagement/ShippingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelcollin.shippingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingManagementApplication.class, args);
+    }
+
+}
---
+++ shipping-management/src/test/java/com/raphaelcollin/shippingmanagement/ShippingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelcollin.shippingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
